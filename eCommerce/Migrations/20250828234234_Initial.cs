46918b53d81e609359c6b29e8dc2ea970cdf3c47
Aiden Richard <115966042+aiden-richard@users.noreply.github.com>
2025-08-28 16:47:31 -0700

    Issue #11 configure db (#15)
    * Add packages for EF Core and Sqlite
    
    * Add BookDbContext for managing Book entities
    
    Introduces the `BookDbContext` class inheriting from `DbContext` to handle the database context for `Book` entities. Includes a constructor for options and a `DbSet<Book>` property to represent the collection of books in the database.
    
    * Add connection string
    
    * Add DB to services
    
    * code first migrations
    
    * Move db location

---
+++ eCommerce/Migrations/20250828234234_Initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    PublishedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ISBN = table.Column<string>(type: "TEXT", maxLength: 13, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
