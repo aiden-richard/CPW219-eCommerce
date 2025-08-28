bcedf4c7db3236ef799bcdd1946a687c477b1014
aiden-richard <richardaiden2006@gmail.com>
2025-08-28 16:30:53 -0700

    Add BookDbContext for managing Book entities
    Introduces the `BookDbContext` class inheriting from `DbContext` to handle the database context for `Book` entities. Includes a constructor for options and a `DbSet<Book>` property to represent the collection of books in the database.

---
+++ eCommerce/Data/BookDbContext.cs
@@ -0,0 +1,13 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data;
+
+public class BookDbContext : DbContext
+{
+    public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+}
