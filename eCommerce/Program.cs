ea15dfa0bf914509d8d121c85d69c12c1cceb101
aiden-richard <richardaiden2006@gmail.com>
2025-08-30 10:02:06 -0700

    Add session support

--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -10,6 +10,17 @@ builder.Services.AddDbContext<eCommerce.Data.BookShopDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+// Add session support
+builder.Services.AddDistributedMemoryCache();
+
+// Configure session options
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +35,8 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapStaticAssets();
 
