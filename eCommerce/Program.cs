68118f6323bd632b8310bcfec510b2e8499b39f8
aiden-richard <richardaiden2006@gmail.com>
2025-08-29 19:02:26 -0700

    Rename Context in Program.cs

--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // Add DB context settings
-builder.Services.AddDbContext<eCommerce.Data.BookDbContext>(options =>
+builder.Services.AddDbContext<eCommerce.Data.BookShopDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
