d822f8ff1d889fa846ead39c340498cc4e519789
aiden-richard <richardaiden2006@gmail.com>
2025-08-29 19:01:40 -0700

    Added View Model for register page and added register functionality

--- eCommerce/Controllers/MemberController.cs
+++ eCommerce/Controllers/MemberController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Controllers;
 
-public class MemberController : Controller
+public class MemberController(BookShopDbContext context) : Controller
 {
+    private readonly BookShopDbContext _context = context;
+
+    [HttpGet]
     public IActionResult Register()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistrationViewModel vm)
+    {
+        if (ModelState.IsValid)
+        {
+            // Map the ViewModel to the Member entity
+            Member newMember = new()
+            {
+                Username = vm.Username,
+                Password = vm.Password,
+                Email = vm.Email,
+                DateOfBirth = vm.DateOfBirth
+            };
+            _context.Members.Add(newMember);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(vm);
     }
 }
